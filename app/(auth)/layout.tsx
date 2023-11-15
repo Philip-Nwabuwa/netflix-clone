6044807ecc19888b32b5f3e2a067637b4a24a270
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-11-15 20:25:09 +0000

    adding images

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,17 @@
 import { ReactNode } from "react";
+import Image from "next/image";
+import BgImage from "@/public/login_background.jpg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src="/login_background.jpg"
+        width={100}
+        height={100}
+        alt="Background Image"
+        className="hidden sm:flex sm:object-cover -z-10"
+      />
       {children}
     </div>
   );
