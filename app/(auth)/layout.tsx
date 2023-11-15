433362f3ff6a0a5680f79c6c253f1caad90b0343
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-11-15 22:06:34 +0000

    Added AuthOptions

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from "react";
 import Image from "next/image";
 import BgImage from "@/public/login_background.jpg";
+import Logo from "@/public/netflix_logo.svg";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Image
-        src="/login_background.jpg"
-        width={100}
-        height={100}
+        src={BgImage}
+        fill
+        priority
         alt="Background Image"
         className="hidden sm:flex sm:object-cover -z-10"
+      />
+      <Image
+        src={Logo}
+        alt="Logo"
+        width={120}
+        height={120}
+        priority
+        className="absolute left-4 top-4 object-contain md:left-10 md:top-10"
       />
       {children}
     </div>
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Github } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import GoogleIcon from "@/public/google.svg";
 
 const page = () => {
-  return <div>page sign in</div>;
+  return (
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
+      <form>
+        <h1 className="text-3xl font-semibold text-white">Log In</h1>
+        <div className="space-y-4 mt-5">
+          <Input
+            type="email"
+            name="email"
+            placeholder="example@gmail.com"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-400 inline-block "
+          />
+          <Button
+            type="submit"
+            variant={"destructive"}
+            className="w-full bg-[#e50914]"
+          >
+            Log In
+          </Button>
+        </div>
+      </form>
+
+      <div className="text-gray-500 text-sm mt-2">
+        New to Netflix
+        <Link className="text-white hover:underline" href="/sign-up">
+          Sign up now!
+        </Link>
+      </div>
+
+      <div className="flex w-full justify-center items-center gap-x-3 mt-6">
+        <Button variant="outline" size="icon">
+          <Image
+            priority
+            src={GoogleIcon}
+            alt={"Google Log in button"}
+            className="w-6 h-6"
+          />
+        </Button>
+        <Button variant="outline" size="icon">
+          <Github className="w-4 h-4" />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default page;
