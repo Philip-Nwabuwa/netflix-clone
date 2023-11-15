0b093af08921ffabd22ae17781c107a27afdbf8e
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-11-15 20:09:35 +0000

    Initialized App

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="m-5">
+      <h1>Hello index</h1>
+      <Button>Click</Button>
+    </main>
+  );
+}
