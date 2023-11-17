31c838e14a696fa07962fc01b2cd0f0924e68793
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2023-11-17 19:56:13 +0000

    update

--- components/common/RecentlyAdded.tsx
+++ components/common/RecentlyAdded.tsx
@@ -42,6 +42,7 @@ const RecentlyAdded = async () => {
 
           <div className="h-60 relative z-10 w-full transform transition duration-500 hover:scale-110 opacity-0 hover:opacity-100">
             <div className="bg-gradient-to-b from-transparent via-black/50 to-black z-10 w-full h-full rounded-lg flex items-center justify-center border">
+              
               <Image
                 src={movie.imageString}
                 alt="Movie"
